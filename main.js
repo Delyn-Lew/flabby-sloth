d0a558dc26403552609b789e894a36bd52f10abd
Delyn Lew <delynlpy@gmail.com>
2024-04-03 17:17:14 +0800

    spacebar will stop working if bear is on ground

--- main.js
+++ main.js
@@ -8,12 +8,21 @@ document.addEventListener('DOMContentLoaded', () => {
    let bearLeft = 160;
    let bearBottom = 120;
    let gravity = 2;
+   let isGameOver = false;
 
    function startGame() {
+    if (bearBottom > 0){
     bearBottom -= gravity;
     bear.style.bottom = bearBottom + "px";
     bear.style.left = bearLeft + "px";
+    treeUp.style.animationPlayState = "running";
+    treeDown.style.animationPlayState = "running";
+    }
+    if (bearBottom <= 0) {
+        gameOver();
+      }
    }
+
 let gameInterval = setInterval(startGame,10);
 
 function moveItwithSpace(e) {
@@ -24,12 +33,22 @@ function moveItwithSpace(e) {
 function jump() {
     if(bearBottom < 500) bearBottom += 50;
     bear.style.bottom = bearBottom + "px";
-    console.log(bearBottom);
+    // console.log(bearBottom);
 }
 document.addEventListener("keyup",moveItwithSpace);
 
-treeUp.addEventListener("animationiteration",()=>{
+treeUp.addEventListener("animationiteration", intoGap)
+
+function intoGap(){
     let gap = ((Math.random() * 300) + 100);
     treeUp.style.height = gap + "px";
-});
+};
+
+function gameOver() {
+    clearInterval(gameInterval)
+    isGameOver = true;
+    document.removeEventListener("keyup", moveItwithSpace)
+    treeUp.style.animationPlayState = "paused"
+    treeDown.style.animationPlayState = "paused"
+}
 })
