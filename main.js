1f0090a8ce101cd669688de84ade8f560b43d135
Delyn Lew <delynlpy@gmail.com>
2024-04-06 23:38:45 +0800

    game over page with restart button functioning but high score not done linking up yet

--- main.js
+++ main.js
@@ -7,90 +7,110 @@ document.addEventListener('DOMContentLoaded', () => {
   const treeDown = document.querySelector('.treeDown');
   const startModal = document.getElementById('startPage');
   const startBtn = document.getElementById('startBtn');
+  const gameEndModal = document.getElementById('gameEnd');
+  const restartBtn = document.getElementById('restartBtn');
 
-// state variables
+  // state variables
   let bearLeft = 50;
   let bearBottom = 350;
   let gravity = 2;
   let isGameOver = false;
   let isRunning = false;
   let score = 0;
+  let gameInterval;
   
   startModal.style.display = "block";
 
-  startBtn.addEventListener("click", function(){
+  startBtn.addEventListener("click", function() {
     startModal.style.display = "none";
     isRunning = true;
-    gameInterval = setInterval(startGame,10);
-    document.addEventListener("keyup",moveItwithSpace);
+    document.addEventListener("keyup", moveItwithSpace);
+    gameInterval = setInterval(startGame, 10)
+    startGame();
   });
 
+  restartBtn.addEventListener("click", function(){
+    gameEndModal.style.display = "none";
+    gameRestart();
+    document.addEventListener("keyup", moveItwithSpace);
+
+  })
+
+  
   function startGame() {
     if (bearBottom > 0){
-    bearBottom -= gravity;
-    bear.style.bottom = bearBottom + "px";
-    bear.style.left = bearLeft + "px";
-    treeUp.style.animationPlayState = "running";
-    treeDown.style.animationPlayState = "running";
-    collisionDetector();
+      bearBottom -= gravity;
+      bear.style.bottom = bearBottom + "px";
+      bear.style.left = bearLeft + "px";
+      treeUp.style.animationPlayState = "running";
+      treeDown.style.animationPlayState = "running";
+      collisionDetector();
     }
     if (bearBottom <= 0) {
-        gameOver();
-      }
-   }
-
-let gameInterval = setInterval(startGame,10);
-
-function moveItwithSpace(e) {
-    if(e.keyCode === 32) {
-        jump();
+      gameOver();
     }
-}
-function jump() {
-    if(bearBottom < 500) bearBottom += 50;
-    bear.style.bottom = bearBottom + "px";
-    // console.log(bearBottom);
-}
-document.addEventListener("keyup",moveItwithSpace);
-
-treeUp.addEventListener("animationiteration", intoGap)
-
-function intoGap(){
-  score++;
-  updateScoreDisplay();
-    let gap = ((Math.random() * 300) + 100);
-    treeUp.style.height = gap + "px";
-};
-
-function gameOver() {
+  }
+  
+  function gameRestart() {
+    isGameOver = false;
+    bearBottom = 350;
+    window.location.reload();
+  }
+  
+  function gameOver() {
     clearInterval(gameInterval)
+    console.log("gameover")
     isGameOver = true;
+    isRunning = false;
     document.removeEventListener("keyup", moveItwithSpace)
     treeUp.style.animationPlayState = "paused"
     treeDown.style.animationPlayState = "paused"
-}
+    gameEndModal.style.display = 'block';
+  }
+  
+  function moveItwithSpace(e) {
+    if(e.keyCode === 32) {
+      jump();
+    }
+  }
+  function jump() {
+    if(bearBottom < 500) bearBottom += 50;
+    bear.style.bottom = bearBottom + "px";
+    // console.log(bearBottom);
+  }
+  document.addEventListener("keyup",moveItwithSpace);
+  
 
-function collisionDetector(){
+  treeUp.addEventListener("animationiteration", intoGap)
+  
+  function intoGap(){
+    let gap = ((Math.random() * 300) + 100);
+    treeUp.style.height = gap + "px";
+    score++;
+    updateScoreDisplay();
+  };
+  
+  function collisionDetector(){
     const bearBounding = bear.getBoundingClientRect();
     const treeUpBounding = treeUp.getBoundingClientRect();
     const treeDownBounding = treeDown.getBoundingClientRect();
-
+    
     if (
       (bearBounding.x < treeUpBounding.x + treeUpBounding.width &&
         bearBounding.x + bearBounding.width > treeUpBounding.x &&
         bearBounding.y < treeUpBounding.y + treeUpBounding.height &&
         bearBounding.y + bearBounding.height > treeUpBounding.y) ||
-      (bearBounding.x < treeDownBounding.x + treeDownBounding.width &&
-        bearBounding.x + bearBounding.width > treeDownBounding.x &&
-        bearBounding.y + bearBounding.height > treeDownBounding.y &&
-        bearBounding.y < treeDownBounding.y + treeDownBounding.height)
-    ) {
-      gameOver();
-    }
-}
-function updateScoreDisplay(){
-  const scoreDisplay = document.getElementById('score');
-  scoreDisplay.textContent = `Score: ${score}`;
-}
-
-})
+        (bearBounding.x < treeDownBounding.x + treeDownBounding.width &&
+          bearBounding.x + bearBounding.width > treeDownBounding.x &&
+          bearBounding.y + bearBounding.height > treeDownBounding.y &&
+          bearBounding.y + bearBounding.height < treeDownBounding.y + treeDownBounding.height)
+          ) {
+            gameOver();
+          }
+        }
+        function updateScoreDisplay(){
+          const scoreDisplay = document.getElementById('score');
+          scoreDisplay.textContent = `Score: ${score}`;
+        }
+        
+      })
